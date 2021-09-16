cf207a479917a05545d19692af133a68c625f068
kevall474 <kevall474@tuta.io>
2021-09-15 22:14:58 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,18 @@ env _cpu_sched=1 makepkg -s
 
 rm -rf pkg src
 
+env _cpu_sched=2 makepkg -s
+
+rm -rf pkg src
+
+env _cpu_sched=3 makepkg -s
+
+rm -rf pkg src
+
+env _cpu_sched=4 makepkg -s
+
+rm -rf pkg src
+
 # clang
 
 env _compiler=2 makepkg -s
@@ -18,5 +30,17 @@ env _compiler=2 makepkg -s
 rm -rf pkg src
 
 env _cpu_sched=1 _compiler=2 makepkg -s
+
+rm -rf pkg src
+
+env _cpu_sched=2 _compiler=2 makepkg -s
+
+rm -rf pkg src
+
+env _cpu_sched=3 _compiler=2 makepkg -s
+
+rm -rf pkg src
+
+env _cpu_sched=4 _compiler=2 makepkg -s
 
 rm -rf pkg src
